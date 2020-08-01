0c21bca2ff1b306d4cae0655c2c0d1a15f1a06bc
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-08-01 20:08:26 +0300

    genre filtration by name added

--- src/main/java/com/Eragoo/Blog/article/genre/GenreRepository.java
+++ src/main/java/com/Eragoo/Blog/article/genre/GenreRepository.java
@@ -1,8 +1,9 @@
 package com.Eragoo.Blog.article.genre;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GenreRepository extends JpaRepository<Genre, Long> {
+public interface GenreRepository extends JpaRepository<Genre, Long>, JpaSpecificationExecutor<Genre> {
 }
